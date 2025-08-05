ae0057f08a46be333536bef913749f932d568dea
Ajit Sharma <ajitsharma@192.168.1.17>
2025-08-06 01:27:33 +0530

    minor updates

--- prime_factors.py
+++ prime_factors.py
@@ -39,13 +39,10 @@ def primeCount(n):
 
 
 if __name__ == '__main__':
-    test_cases = [0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
-                  20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,
-                  35,36,37,38,39,40,41,42,43,44,45,46,47,48,
-                  49,50]
+    test_cases = [0,1,2,3,5,10,20,50,100,500,10000,10000,100000000]
 
     for n in test_cases:
-        result2 = primeCount(n)
+        result = primeCount(n)
         # print(f"Result for {n} (primeCount): {result}")
-        print(f"Result for {n} (primeFactorsCount): {result2}")
+        print(f"Result for {n} (primeFactorsCount): {result}")
         
