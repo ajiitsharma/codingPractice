c9ed6ad605ded7fbde04b4417d6a922b8c46dd66
Ajit Sharma <ajitsharma@192.168.1.17>
2025-08-06 15:22:39 +0530

    reverse game logic updated

--- reverse_game.py
+++ reverse_game.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3 
 
 '''
-Akash and Akhil are playing a game. They have  balls numbered from  to . Akhil asks Akash to reverse the position of the balls, i.e., to 
-change the order from say, 0,1,2,3 to 3,2,1,0. He further asks Akash to reverse the position of the balls  times, each time starting from one position 
-further to the right, till he reaches the last ball. So, Akash has to reverse the positions of the ball starting from  position, then from  position,
-then from  position and so on. At the end of the game, Akhil will ask Akash the final position of any ball numbered . 
-Akash will win the game, if he can answer. Help Akash.
+        Akash and Akhil are playing a game. They have  balls numbered from  to . Akhil asks Akash to reverse the position of the balls, i.e., to 
+        change the order from say, 0,1,2,3 to 3,2,1,0. He further asks Akash to reverse the position of the balls  times, each time starting from one position 
+        further to the right, till he reaches the last ball. So, Akash has to reverse the positions of the ball starting from  position, then from  position,
+        then from  position and so on. At the end of the game, Akhil will ask Akash the final position of any ball numbered . 
+        Akash will win the game, if he can answer. Help Akash.
 
-Write a function `reverse_game` that takes an integer `n` and returns the final position of the ball numbered `n` after performing the described operations.
+        Write a function `reverse_game` that takes an integer `n` and returns the final position of the ball numbered `n` after performing the described operations.
+'''
+
+'''
+        For N balls, the final position of the balls are N-1, 0, N-2, 1, N-3, 2, ..., N//2, N//2+1 if N is even
+        and N-1, 0, N-2, 1, N-3, 2, ..., N//2+1, N//2 if N is odd.
+        
 '''
 
 import math
@@ -15,13 +21,18 @@ import math
 def reverse_game(n, k):
     # Create a list of balls
     balls = list(range(n))
+
+    finalPosition = []
+    for i in range((n+1)//2):
+        finalPosition.append(balls[n - 1 - i])
+
+        if i != n - 1 - i:  # Avoid adding the same element twice in case of odd n
+            finalPosition.append(balls[i])
     
-    # Perform the swapping operations n times. Each time swap the ith ball with the last ball
-    for i in range(n):
-        balls[i], balls[n-1] = balls[n-1], balls[i]
+    print(f"Final positions after reversing {n} balls: {finalPosition}")
     
     # Return the final position of the ball numbered k
-    return balls.index(k)+1
+    return finalPosition.index(k)
 
 if __name__ == '__main__':
     # Test cases
