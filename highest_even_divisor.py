77d13e4126589e5d75df89b2afe0d16b979a8fef
ajiitsharma <ajitsharma.iitkgp@gmail.com>
2025-08-07 17:46:15 +0530

    divisor logic update

--- highest_even_divisor.py
+++ highest_even_divisor.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+def divisors(n):
+    # Write your code here
+    count = 0
+    for j in range(2, n//2 +1, 2):
+        if n % j == 0 and j % 2 == 0    :
+            count += 1
 
+    return count
 
 
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
-    result = highest_even_divisor(num)
+    result = divisors(num)
     if result:
-        print(f"The highest even divisor of {num} is {result}.")
+        print(f"Number of even divisors of {num} is {result}.")
     else:
         print(f"{num} has no even divisors.")
