af01942622863712c244da9df679fc4a59a9e24e
ajiitsharma <ajitsharma.iitkgp@gmail.com>
2025-08-14 20:20:51 +0530

    prime function added

--- primitive_roots.py
+++ primitive_roots.py
@@ -6,9 +6,23 @@ import random
 import re
 import sys
 
+def isPrime(n: int) -> bool:
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 ==0 or n% 3 == 0:
+        return False
+    
+    i = 5
+    while i*i <= n:
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+        i = i + 6
 
+    return True
 
-def primitive_roots(n:int) -> list[int]:
+def primitive_roots(p:int) -> list[int]:
     candidates = []
 
     residues = set(range(1, p))
